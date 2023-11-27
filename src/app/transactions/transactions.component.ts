710ecabfbc99989a712ffb3ad266fb38d1e47c30
chhrahul <chh_rahul@live.com>
2023-11-27 18:10:51 +0530

    updated changes in neo capital banking app on transactions page

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { MyModalComponent } from '../my-modal/my-modal.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
+import {FormGroup, FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+// import {MatTabsModule} from '@angular/material/tabs';
+// import {MatDatepickerModule} from '@angular/material/datepicker';
 // Import Angular Material modules
+
+const today = new Date();
+const month = today.getMonth();
+const year = today.getFullYear();
+
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
-  styleUrls: ['./transactions.component.scss']
+  styleUrls: ['./transactions.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class TransactionsComponent {
@@ -19,4 +27,12 @@ export class TransactionsComponent {
   toggleCardVisibility() {
     this.isCardVisible = !this.isCardVisible;
   }
+  campaignOne = new FormGroup({
+    start: new FormControl(new Date(year, month, 13)),
+    end: new FormControl(new Date(year, month, 16)),
+  });
+  campaignTwo = new FormGroup({
+    start: new FormControl(new Date(year, month, 15)),
+    end: new FormControl(new Date(year, month, 19)),
+  });
 }
